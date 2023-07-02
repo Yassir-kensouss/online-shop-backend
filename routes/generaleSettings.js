50fe06089b058406fa04d10528cf69be7bac7088
yqssirkensouss <y.kensouss@qalqul.io>
2023-07-02 16:01:10 -0500

    updates

--- routes/generaleSettings.js
+++ routes/generaleSettings.js
@@ -2,15 +2,20 @@ const express = require("express");
 const {
   fetchGeneraleSettings,
   saveGeneraleSettings,
-  updateGeneraleSettings
+  updateGeneraleSettings,
 } = require("../controllers/generaleSettingController");
 const { getGeneraleSettings } = require("../middlewares/generaleSetting");
+const { isAuth, isAdmin, requireSignIn } = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/general", fetchGeneraleSettings);
-router.post("/general", saveGeneraleSettings);
-router.put("/general/:settingsId", updateGeneraleSettings);
+router.post("/general", [requireSignIn, isAuth, isAdmin], saveGeneraleSettings);
+router.put(
+  "/general/:settingsId",
+  [requireSignIn, isAuth, isAdmin],
+  updateGeneraleSettings
+);
 
-router.param("settingsId", getGeneraleSettings)
+router.param("settingsId", getGeneraleSettings);
 
 module.exports = router;
